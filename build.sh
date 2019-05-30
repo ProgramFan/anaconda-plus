bab4ce1efa4b2d03f55288cb1d47cd22fd372161
Yang Zhang <yang_zhang@iapcm.ac.cn>
2019-05-31 00:50:58 +0800

    Fix plotly offline mode

--- build.sh
+++ build.sh
@@ -4,6 +4,9 @@
 export https_proxy=http://127.0.0.1:1080
 export http_proxy=${https_proxy}
 CONSTRUCTOR=~/.local/opt/miniconda3/bin/constructor
+DATE=$(date +%Y.%m.%d)
+
+sed -ie "s|version:.*$|version: $DATE|g" python[23]/construct.yaml
 
 ${CONSTRUCTOR} --platform=linux-64 --verbose python2
 ${CONSTRUCTOR} --platform=linux-64 --verbose python3
