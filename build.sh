69bf299f705f92a8b378e237a56cde454d31226f
Yang Zhang <yang_zhang@iapcm.ac.cn>
2019-08-13 08:57:06 +0800

    Upgrade to constructor 3.0 (only python3 compiles)

--- build.sh
+++ build.sh
@@ -4,9 +4,10 @@
 export https_proxy=http://127.0.0.1:1080
 export http_proxy=${https_proxy}
 CONSTRUCTOR=~/.local/opt/miniconda3/bin/constructor
+CONSTRUCTOR_ARGS="--conda-exe conda-exe/conda-4.7.5-linux-64.exe"
 DATE=$(date +%Y.%m.%d)
 
 sed -ie "s|version:.*$|version: $DATE|g" python[23]/construct.yaml
 
-${CONSTRUCTOR} --platform=linux-64 --verbose python2
-${CONSTRUCTOR} --platform=linux-64 --verbose python3
+${CONSTRUCTOR} ${CONSTRUCTOR_ARGS} --platform=linux-64 --verbose python2
+${CONSTRUCTOR} ${CONSTRUCTOR_ARGS} --platform=linux-64 --verbose python3
