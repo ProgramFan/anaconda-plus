33a420d9aab1113774526ffbd25ca659505ddf0d
Yang Zhang <yang_zhang@iapcm.ac.cn>
2019-12-26 07:47:35 +0800

    Upgrade conda to 4.8.0 from conda-forge

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@
 export https_proxy=http://127.0.0.1:1080
 export http_proxy=${https_proxy}
 CONSTRUCTOR=~/.local/opt/miniconda3/bin/constructor
-CONSTRUCTOR_ARGS="--conda-exe conda-exe/conda-4.7.5-linux-64.exe"
+CONSTRUCTOR_ARGS="--conda-exe standalone-conda/conda.exe"
 DATE=$(date +%Y.%m.%d)
 
 sed -ie "s|version:.*$|version: $DATE|g" python[23]/construct.yaml
