d50134dc05f35141d27357c71edc850a1c1e0474
Yang Zhang <yang_zhang@iapcm.ac.cn>
2020-01-11 01:09:31 +0800

    Update anaconda3-plus recipe

--- build.sh
+++ build.sh
@@ -16,3 +16,5 @@ function build_for_arch() {
 
 build_for_arch miniconda3-plus linux-64
 build_for_arch miniconda3-plus linux-aarch64
+build_for_arch anaconda3-plus linux-64
+build_for_arch anaconda3-plus linux-aarch64
