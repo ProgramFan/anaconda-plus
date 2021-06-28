b08a7205600a2c64cf61917c982995635c85881e
Yang Zhang <yang_zhang@iapcm.ac.cn>
2021-06-28 12:44:05 +0800

    fix: fix build script  on macos

--- build.sh
+++ build.sh
@@ -9,12 +9,12 @@ function build_for_arch() {
   local pkg=$1
   local platform=$2
   local date=$(date +%Y.%m.%d)
-  gsed -ie "s|version:.*$|version: $date|g" "${pkg}/construct.yaml"
+  gsed -i "s|version:.*$|version: $date|g" "${pkg}/construct.yaml"
   ${CONSTRUCTOR} --conda-exe "standalone-conda/conda-${platform}.exe" \
     "--platform=${platform}" --verbose "$pkg"
 }
 
-build_for_arch miniconda3-plus linux-64
-build_for_arch miniconda3-plus linux-aarch64
+# build_for_arch miniconda3-plus linux-64
+# build_for_arch miniconda3-plus linux-aarch64
 build_for_arch anaconda3-plus linux-64
 # build_for_arch anaconda3-plus linux-aarch64
