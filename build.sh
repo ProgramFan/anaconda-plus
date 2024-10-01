75c27834ba1579e7d4501282d962899d0aea6fbc
progranfan <zyangmath@gmail.com>
2024-10-01 23:15:26 +0800

    feat: update anaconda3-plus for new conda-forge

--- build.sh
+++ build.sh
@@ -1,18 +1,14 @@
 #!/bin/bash
 #
 
-# export https_proxy=http://127.0.0.1:1080
-# export http_proxy=${https_proxy}
 CONSTRUCTOR=~/.local/opt/anaconda3-plus/bin/constructor
 
 function build_for_arch() {
   local pkg=$1
   local platform=$2
-  local date=$(date +%Y.%m.%d)
+  local date=$(date +%Y.%-m.%-d)
   sed -i "s|version:.*$|version: $date|g" "${pkg}/construct.yaml"
-  # ${CONSTRUCTOR} --conda-exe "standalone-conda/conda-${platform}.exe" \
-  #   "--platform=${platform}" --verbose "$pkg"
-  ${CONSTRUCTOR} --verbose "$pkg"
+  ${CONSTRUCTOR} "$pkg"
 }
 
 # build_for_arch miniconda3-plus linux-64
